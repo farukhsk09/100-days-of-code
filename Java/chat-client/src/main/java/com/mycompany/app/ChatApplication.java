ab1840434ca5b3d766ad9accbc3b6f2a8e4c9bf6
farukhsk09 <farukh.sk09@gmail.com>
2022-03-10 19:38:31 +0530

    Added integration to local dynamoDB - insert&get

--- Java/chat-client/src/main/java/com/mycompany/app/ChatApplication.java
+++ Java/chat-client/src/main/java/com/mycompany/app/ChatApplication.java
@@ -1,10 +1,16 @@
 package com.mycompany.app;
 
+import java.net.URI;
+import java.net.URISyntaxException;
+
 import com.mycompany.app.resources.CreateTableResource;
 
 import io.dropwizard.Application;
 import io.dropwizard.setup.Bootstrap;
 import io.dropwizard.setup.Environment;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.dynamodb.DynamoDbAsyncClient;
+import software.amazon.awssdk.services.dynamodb.DynamoDbClient;
 
 public class ChatApplication extends Application<ChatConfiguration> {
 
@@ -24,9 +30,18 @@ public class ChatApplication extends Application<ChatConfiguration> {
 
     @Override
     public void run(final ChatConfiguration configuration,
-                    final Environment environment) {
+                    final Environment environment) throws URISyntaxException {
         // TODO: implement application
-        final CreateTableResource resource = new CreateTableResource();
+        Region region = Region.US_EAST_1;
+        DynamoDbAsyncClient ddb = DynamoDbAsyncClient.builder()
+                .endpointOverride(new URI("http://localhost:8000"))
+                .region(region)
+                .build();
+        DynamoDbClient ddbsync = DynamoDbClient.builder()
+                .endpointOverride(new URI("http://localhost:8000"))
+                .region(region)
+                .build();
+        final CreateTableResource resource = new CreateTableResource(ddb,ddbsync);
         environment.jersey().register(resource);
     }
 
--- Java/chat-client/src/main/java/com/mycompany/app/resources/CreateTableResource.java
+++ Java/chat-client/src/main/java/com/mycompany/app/resources/CreateTableResource.java
@@ -1,7 +1,14 @@
 package com.mycompany.app.resources;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
 import java.util.UUID;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+import java.util.stream.Collectors;
 
+import javax.inject.Inject;
 import javax.ws.rs.HeaderParam;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
@@ -14,6 +21,15 @@ import com.mycompany.app.api.User;
 import org.eclipse.jetty.util.log.Log;
 import org.eclipse.jetty.util.log.Logger;
 
+import software.amazon.awssdk.services.dynamodb.DynamoDbAsyncClient;
+import software.amazon.awssdk.services.dynamodb.DynamoDbClient;
+import software.amazon.awssdk.services.dynamodb.model.AttributeValue;
+import software.amazon.awssdk.services.dynamodb.model.DescribeTableRequest;
+import software.amazon.awssdk.services.dynamodb.model.GetItemRequest;
+import software.amazon.awssdk.services.dynamodb.model.GetItemResponse;
+import software.amazon.awssdk.services.dynamodb.model.PutItemRequest;
+import software.amazon.awssdk.services.dynamodb.model.PutItemResponse;
+
 
 
 
@@ -21,15 +37,60 @@ import org.eclipse.jetty.util.log.Logger;
 @Produces(MediaType.APPLICATION_JSON)
 public class CreateTableResource{
     Logger logger = Log.getLogger(CreateTableResource.class);
+    private DynamoDbAsyncClient ddb;
+    private DynamoDbClient ddbsync;
+    public CreateTableResource(DynamoDbAsyncClient ddb,DynamoDbClient ddbsync){
+        this.ddb=ddb;
+        this.ddbsync=ddbsync;
+    }
 
     @Path("/user")
     @POST
-    public void createUser(User user,@HeaderParam("Token") String token){
+    public void createUser(User user,@HeaderParam("Token") String token) throws ExecutionException{
         logger.info("recieved a user "+user.toString());
         //validate token using the go client
         //insert user into dynamoDB 
         String primaryKey = "USER#"+user.getUsername();
         String sortKey = "PASSWORD#"+user.getPassword();
+        //create the item
+        Map<String, AttributeValue> item = new HashMap<>();
+        item.put("PKey", AttributeValue.builder().s(primaryKey).build());
+        item.put("SKey", AttributeValue.builder().s(sortKey).build());
+        item.put("CreatedOn", AttributeValue.builder().s(String.valueOf(System.currentTimeMillis())).build());
+
+        //create the putitemrequest
+        PutItemRequest putItemRequest;
+        putItemRequest = PutItemRequest.builder()
+                            .tableName("MESSAGES_USER_TABLE")
+                            .item(item)
+                            .build();
+        Map<String, AttributeValue> keyMap=new HashMap<>();
+        keyMap.put("PKey", AttributeValue.builder().s(primaryKey).build());
+        keyMap.put("SKey", AttributeValue.builder().s(sortKey).build());
+        CompletableFuture<PutItemResponse> response = this.ddb.putItem(putItemRequest);
+        response.join();
+        /*CompletableFuture<PutItemResponse> response = this.ddb.putItem(putItemRequest);
+        response.join();
+        
+        
+        GetItemRequest getItemRequest = GetItemRequest.builder().tableName("MESSAGES_USER_TABLE").key(keyMap).build();
+        CompletableFuture<GetItemResponse> responseget = this.ddb.getItem(getItemRequest);
+        try {
+            GetItemResponse finalresponse = responseget.get();
+            System.out.println(finalresponse.item().get(primaryKey).s());
+        } catch (InterruptedException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }*/
+        // ddbsync.putItem(putItemRequest);
+        GetItemRequest getItemRequest = GetItemRequest.builder().tableName("MESSAGES_USER_TABLE").key(keyMap).build();
+        GetItemResponse getresponse = ddbsync.getItem(getItemRequest);
+        Map<String, AttributeValue> map=getresponse.item().values().stream().collect(Collectors.toMap(AttributeValue::s, s->s));
+        Set<String> keys = map.keySet();
+            for (String sinKey : keys) {
+                System.out.format("%s: %s\n", sinKey, map.get(sinKey).toString());
+            }
+        System.out.println(getresponse.item().entrySet().size());
         
         return;
     }
