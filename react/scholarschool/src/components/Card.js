e62f2861f684bec674e38b8db07f84b681d80a04
farukhsk09 <farukh.sk09@gmail.com>
2022-03-17 22:56:18 +0530

    icons better way

--- react/scholarschool/src/components/Card.js
+++ react/scholarschool/src/components/Card.js
@@ -2,10 +2,17 @@ import './Card.css';
 function Card(props){
     return (
         <div className='card'>
-            <button>{props.button}</button>
+            {/* <button>{props.button}</button> */}
+            <button><IcSharpPlus></IcSharpPlus></button>
             <h5>{props.premise}</h5>
         </div>
     );
 }
 
+
+export function IcSharpPlus(props) {
+    return (
+      <svg width="1em" height="1em" viewBox="0 0 24 24" {...props}><path fill="currentColor" d="M19 12.998h-6v6h-2v-6H5v-2h6v-6h2v6h6z"></path></svg>
+    )
+  }
 export default Card;
--- react/scholarschool/src/components/Content.js
+++ react/scholarschool/src/components/Content.js
@@ -6,7 +6,7 @@ function Content() {
             <div className='top-layer'>
                 <div className='sub-layer'>
                     <input></input>
-                    <div className='notification'>! Duress</div>
+                    <div className='notification'><PhWarningOctagonFill></PhWarningOctagonFill>Duress</div>
                 </div>
                 <div className='profile'>
                     
@@ -26,5 +26,13 @@ function Content() {
         </div>
     );
 }
+
+
+
+export function PhWarningOctagonFill(props) {
+    return (
+      <svg width="1em" height="1em" viewBox="0 0 256 256" {...props}><path fill="currentColor" d="m227.3 80.2l-51.5-51.5a16.1 16.1 0 0 0-11.3-4.7h-73a16.1 16.1 0 0 0-11.3 4.7L28.7 80.2A16.1 16.1 0 0 0 24 91.5v72.9a16.1 16.1 0 0 0 4.7 11.4l51.5 51.5a16.1 16.1 0 0 0 11.3 4.7h73a16.1 16.1 0 0 0 11.3-4.7l51.5-51.5a16.1 16.1 0 0 0 4.7-11.4V91.5a16.1 16.1 0 0 0-4.7-11.3ZM120 80a8 8 0 0 1 16 0v56a8 8 0 0 1-16 0Zm8 104a12 12 0 1 1 12-12a12 12 0 0 1-12 12Z"></path></svg>
+    )
+  }
 
 export default Content;
